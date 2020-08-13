d056d81a0a1f910af50776c44ff7420cd49dd841
devidy <devidy.oliveira@gmail.com>
2020-08-13 20:53:44 -0300

    fix: adiciona id no put de funcionario

--- routes/api.php
+++ routes/api.php
@@ -21,5 +21,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::get('/funcionarios', 'FuncionarioController@index');
 Route::get('/funcionarios/{funcionario}', 'FuncionarioController@show');
 Route::post('/funcionarios', 'FuncionarioController@store');
-Route::put('/funcionarios', 'FuncionarioController@update');
+Route::put('/funcionarios/{funcionario}', 'FuncionarioController@update');
 Route::delete('/funcionarios', 'FuncionarioController@destroy');
