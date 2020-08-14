aa2775b4a781cfa2a5a9cb1020f9c7e1a021dc50
devidy <devidy.oliveira@gmail.com>
2020-08-14 13:58:33 -0300

    feat: formata data com serialização

--- app/Models/Funcionario.php
+++ app/Models/Funcionario.php
@@ -6,5 +6,22 @@ use Illuminate\Database\Eloquent\Model;
 
 class Funcionario extends Model
 {
+    /**
+     * Faz conversão na hora da serialização
+     *
+     * @var array
+     */
     protected $fillable = ['name', 'cpf', 'admissao', 'status'];
+
+    /**
+     * Faz conversão na hora da serialização
+     *
+     * @var array
+     */
+    protected $casts = [
+        'admissao' => 'datetime:d-m-Y H:00',
+        'demissao' => 'datetime:d-m-Y H:00',
+        'created_at' => 'datetime:d-m-Y H:00',
+        'updated_at' => 'datetime:d-m-Y H:00'
+    ];
 }
