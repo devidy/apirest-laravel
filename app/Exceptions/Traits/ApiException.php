ec62a0d83e927289ace8abaa6d38544165d86d5e
devidy <devidy.oliveira@gmail.com>
2020-08-13 23:55:08 -0300

    feat: add trait para tratar exception e implementa metodos para tratar erro 404

---
+++ app/Exceptions/Traits/ApiException.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Exceptions\Traits;
+
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+trait ApiException {
+
+    /**
+     * Trata as exceções da API
+     *
+     * @param [type] $request
+     * @param [type] $e
+     * @return void
+     */
+    public function getJsonException($request, $e)
+    {
+        if ($e instanceof ModelNotFoundException) {
+            return $this->notFoundException();
+        }
+    }
+
+    /**
+     * Retorna o erro
+     *
+     * @return void
+     */
+    public function notFoundException()
+    {
+        return $this->getResponse(
+            "Recurso não encontrado",
+            "01",
+            404
+        );
+    }
+
+    /**
+     * Monta a resposta em json
+     *
+     * @param [type] $message
+     * @param [type] $code
+     * @param [type] $status
+     * @return void
+     */
+    public function getResponse($message, $code, $status)
+    {
+        return response()->json([
+            "errors" => [
+                [
+                    "status" => $status,
+                    "code" => $code,
+                    "message" => $message
+                ]
+            ]
+        ], $status);
+    }
+}
